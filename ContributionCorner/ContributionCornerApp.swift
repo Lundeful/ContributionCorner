52c89d17fedd76061af56115f60a281ab4173ab6
Christoffer Lund <chrislund90@gmail.com>
2023-01-17 13:32:22 +0100

    Initial Commit

---
+++ ContributionCorner/ContributionCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContributionCornerApp.swift
+//  ContributionCorner
+//
+//  Created by Christoffer Lund on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContributionCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
