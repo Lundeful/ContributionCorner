006d10f39f3c1103abf8f00716427476e67d0629
Christoffer Lund <chrislund90@gmail.com>
2023-02-20 02:58:24 +0100

    Use NSPopover. Remove features only available in MacOS 13.0+

--- ContributionCorner/ContributionCornerApp.swift
+++ ContributionCorner/ContributionCornerApp.swift
@@ -6,25 +6,58 @@
 //
 
 import SwiftUI
-import FluidMenuBarExtra
 
 @main
 struct ContributionCornerApp: App {
-    @NSApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         Settings {
-            Text("Settings are located by pressing the gear icon inside the app")
+            EmptyView()
+//            SettingsView()
         }
     }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private var menuBarExtra: FluidMenuBarExtra?
+    var statusItem: NSStatusItem?
+    var popover = NSPopover()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        self.menuBarExtra = FluidMenuBarExtra(title: "Contribution Corner", systemImage: "square.grid.3x3.fill") {
-            ContentView()
+        popover.behavior = .transient
+        popover.animates = true
+
+        popover.contentViewController = NSViewController()
+        popover.contentViewController?.view = NSHostingView(rootView: ContributionsView())
+        popover.contentSize = .init(width: 900, height: 500)
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let menuButton = statusItem?.button {
+            menuButton.image = NSImage(systemSymbolName: "square.grid.3x3.fill", accessibilityDescription: nil)
+            menuButton.action = #selector(togglePopover)
         }
+    }
+
+    @objc func togglePopover(sender: AnyObject) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover()
+        }
+    }
+
+    func showPopover() {
+        if let menuButton = statusItem?.button {
+            self.popover.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: NSRectEdge.minY)
+            
+            // This makes it close when clicking outside of the popover
+            // TODO: Allow settings change to disable this feature
+            popover.contentViewController?.view.window?.makeKey()
+        }
+    }
+    
+    func closePopover(sender: AnyObject) {
+
     }
 }
