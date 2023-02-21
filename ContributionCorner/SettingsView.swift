21001c1d8c2331c45a3f9892fad9169d42fdcbfe
Christoffer Lund <chrislund90@gmail.com>
2023-02-21 18:48:50 +0100

    Added LaunchAtStartup functionality for MacOS 13+. Set version V1.2

--- ContributionCorner/SettingsView.swift
+++ ContributionCorner/SettingsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ServiceManagement
 
 struct SettingsView: View {
     @State private var showContributionCount: Bool
@@ -13,6 +14,7 @@ struct SettingsView: View {
     @State private var username: String
     @State private var pollingRate: Double
     @State private var hasError = false
+    @State private var launchOnStartup: Bool = false
     @State private var errorMessage = ""
 
     let onDismiss: () -> Void
@@ -30,11 +32,17 @@ struct SettingsView: View {
                 Text("Settings")
                     .font(.title)
                 TextField("GitHub username", text: $username)
+                TextField("Update rate (minutes)", value: $pollingRate, format: .number)
                 Toggle("Display username", isOn: $showUsername)
                     .toggleStyle(.switch)
                 Toggle("Display contribution count", isOn: $showContributionCount)
                     .toggleStyle(.switch)
-                TextField("Update rate (minutes)", value: $pollingRate, format: .number)
+
+                if #available(macOS 13, *) {
+                    Toggle("Launch on startup", isOn: $launchOnStartup)
+                        .toggleStyle(.switch)
+                }
+
                 HStack {
                     Button("Quit app") {
                         NSRunningApplication.current.terminate()
@@ -50,17 +58,22 @@ struct SettingsView: View {
             .alert("Invalid form", isPresented: $hasError) { } message: {
                 Text(errorMessage)
             }
+            .onAppear {
+                // Somehow this wouldn't trigger properly inside the init function, but works here
+                // TODO: Figure out why
+                if #available(macOS 13, *) {
+                    self.launchOnStartup = SMAppService.mainApp.status == .enabled
+                }
+            }
     }
 
     func cancel() {
-        self.showContributionCount = UserDefaults.standard.bool(forKey: "showContributionCount")
-        self.showUsername = UserDefaults.standard.bool(forKey: "showUsername")
-        self.username =  UserDefaults.standard.string(forKey: "username") ?? ""
-        self.pollingRate = UserDefaults.standard.double(forKey: "pollingRate")
         onDismiss()
     }
 
     func save() {
+        clearError()
+
         let trimmedUsername = username.trimmingCharacters(in: .whitespacesAndNewlines)
         guard !trimmedUsername.isEmpty else {
             errorMessage = "Username cannot be empty"
@@ -69,7 +82,7 @@ struct SettingsView: View {
         }
 
         guard pollingRate >= 5 else {
-            errorMessage = "Polling rate must be 5 minutes or higher"
+            errorMessage = "Polling rate must be 5 minutes or higher to prevent spam"
             hasError = true
             pollingRate = 5
             return
@@ -79,7 +92,54 @@ struct SettingsView: View {
         UserDefaults.standard.set(showUsername, forKey: "showUsername")
         UserDefaults.standard.set(trimmedUsername, forKey: "username")
         UserDefaults.standard.set(pollingRate, forKey: "pollingRate")
-        onDismiss()
+
+        updateLaunchAtStartup()
+
+        if errorMessage.isEmpty && !hasError {
+            onDismiss()
+        }
+    }
+
+    func updateLaunchAtStartup() {
+        if #available(macOS 13, *) {
+            let currentStatus = SMAppService.mainApp.status
+
+            if launchOnStartup && currentStatus != .enabled {
+                enableLaunchAtStartup()
+            } else if !launchOnStartup && currentStatus == .enabled {
+                disableLaunchAtStartup()
+            }
+        }
+    }
+
+    func enableLaunchAtStartup() {
+        if #available(macOS 13, *) {
+            do {
+                try SMAppService.mainApp.register()
+            } catch {
+                setError(withMessage: "Failed to add launch at login: " + error.localizedDescription)
+            }
+        }
+    }
+
+    func disableLaunchAtStartup() {
+        if #available(macOS 13, *) {
+            do {
+                try SMAppService.mainApp.unregister()
+            } catch {
+                setError(withMessage: "Failed to remove launch at login: " + error.localizedDescription)
+            }
+        }
+    }
+
+    func setError(withMessage message: String) {
+        errorMessage = message
+        hasError = true
+    }
+
+    func clearError() {
+        errorMessage = ""
+        hasError = false
     }
 }
 
