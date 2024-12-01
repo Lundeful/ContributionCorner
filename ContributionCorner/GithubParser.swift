6407cb4b8a79f62211f29b77eec0203a825cb64c
Christoffer Lund <chrislund90@gmail.com>
2024-12-01 03:37:37 +0100

    Update failing request

--- ContributionCorner/GithubParser.swift
+++ ContributionCorner/GithubParser.swift
@@ -1,53 +1,89 @@
-//
-//  GithubParser.swift
-//  ContributionCorner
-//
-//  Created by Christoffer Lund on 17/01/2023.
-//
-
 import Foundation
 import SwiftSoup
 
 enum NetworkError: Error {
     case badURL
     case parseError
+    case networkError(Error)
+    case invalidResponse
 }
 
 struct GithubParser {
-    static let baseUrl = "http://github.com/"
+    static let baseUrl = "https://github.com/%@?action=show&controller=profiles&tab=contributions&user_id=%@"
 
     static func getLastYearsContributionsAsDates(for user: String, completionHandler: @escaping (Result<[Date], NetworkError>) -> Void) async {
-        guard let url = URL(string: baseUrl + user) else {
-            print("Invalid URL")
+        let urlString = String(format: baseUrl, user.lowercased(), user.lowercased())
+        guard let url = URL(string: urlString) else {
             completionHandler(.failure(.badURL))
             return
         }
 
         do {
-            let html = try String(contentsOf: url)
+            let html = try await fetchHTML(url: url)
             let document = try SwiftSoup.parse(html)
+
             let contributions = try document.select("td.ContributionCalendar-day")
 
-            let mappedContributions: [Date] = try contributions.flatMap { day in
-                let spanContent = try day.select("span.sr-only").html()
-                let contributionCount = Int(spanContent.split(separator: " ")[0]) ?? 0
-                if contributionCount == 0 { return [Date]() }
+            var mappedContributions: [Date] = []
 
-                let date = try parseDate(date: day.attr("data-date"))!
-                return [Date](repeating: date, count: contributionCount)
+            for day in contributions {
+                do {
+                    let dateString = try day.attr("data-date")
+                    guard let date = parseDate(date: dateString) else { continue }
+
+                    let dayId = try day.attr("id")
+                    let tooltip = try document.select("tool-tip[for='\(dayId)']").first()
+                    if let tooltipText = try tooltip?.text() {
+                        let count = extractContributionCount(from: tooltipText)
+                        if count > 0 {
+                            mappedContributions += Array(repeating: date, count: count)
+                        }
+                    }
+                } catch {
+                    print("Debug: Error processing day: \(error)")
+                }
             }
 
             completionHandler(.success(mappedContributions))
+
         } catch {
             completionHandler(.failure(.parseError))
-            print("Error while parsing")
         }
     }
-    
+
+    static func fetchHTML(url: URL) async throws -> String {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("XMLHttpRequest", forHTTPHeaderField: "X-Requested-With")
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200,
+              let html = String(data: data, encoding: .utf8) else {
+            throw NetworkError.invalidResponse
+        }
+
+        return html
+    }
+
     static func parseDate(date: String) -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         return dateFormatter.date(from: date)
     }
-}
 
+    static func extractContributionCount(from tooltipText: String) -> Int {
+        if tooltipText.starts(with: "No contributions") {
+            return 0
+        }
+
+        let components = tooltipText.components(separatedBy: " ")
+        if let countString = components.first,
+           let count = Int(countString) {
+            return count
+        }
+
+        return 0
+    }
+}
